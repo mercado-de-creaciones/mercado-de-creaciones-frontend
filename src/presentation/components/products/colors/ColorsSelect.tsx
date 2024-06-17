c6a30123ac26b946da68bdb3f70e61aaf83eaa5b
xavier777dev <xavier.basurto.777@gmail.com>
2024-06-16 20:29:16 -0500

    selectcolors edit size

--- src/presentation/components/products/colors/ColorsSelect.tsx
+++ src/presentation/components/products/colors/ColorsSelect.tsx
@@ -7,18 +7,18 @@ interface ColorSelectProps {
   colorHex: string;
 }
 
-const ColorsSelect = ({colorName, colorHex}: ColorSelectProps) => {
+const ColorsSelect = ({ colorName, colorHex }: ColorSelectProps) => {
   const [isActive, setIsActive] = useState(false);
 
   const handleClick = () => {
     setIsActive(!isActive);
   }
-  
+
 
   return (
-    <div style={{backgroundColor: colorHex}} className={`${colorName == "white" && "border-2 border-gray-300"} w-8 h-8 rounded-full text-center flex justify-center items-center`} onClick={handleClick}>
+    <div style={{ backgroundColor: colorHex }} className={`${colorName == "white" && "border-2 border-gray-300"} w-[30px] h-[30px] rounded-full text-center flex justify-center items-center`} onClick={handleClick}>
       {
-        (isActive) && ((colorName == "white") ? <Check color="black"/> : <Check color="white"/>) 
+        (isActive) && ((colorName == "white") ? <Check color="black" size={18} /> : <Check color="white" size={18} />)
       }
     </div>
   )
